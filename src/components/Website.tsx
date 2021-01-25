cb49f6e82c961185e9cb35d7f48eb0e39eadc233
BurnySc2 <gamingburny@gmail.com>
2021-01-25 05:01:45 +0100

    First working version

---
+++ src/components/Website.tsx
@@ -0,0 +1,86 @@
+import { allItemNames, generateItemsFrom } from "../constants/helper"
+import React, { useState } from "react"
+
+export default function Website(props: any) {
+    let [allowSmelting, setAllowSmelting] = useState(false)
+    let [inputItems, setInputItems] = useState(Array(20).fill(""))
+
+    let inputHtml = []
+    for (let i = 0; i < 20; i++) {
+        inputHtml.push(
+            <label key={`label${i}`} className={"my-1 mx-4 col-span-2"} htmlFor={`input${i}`}>
+                Input {i + 1}
+            </label>
+        )
+        inputHtml.push(
+            <input
+                className={"bg-blue-200 rounded col-span-3"}
+                key={`input${i}`}
+                id={`input${i}`}
+                list={"items"}
+                value={inputItems[i]}
+                onChange={(e) => {
+                    setInputItems([
+                        ...inputItems.slice(0, i),
+                        e.target.value,
+                        ...inputItems.slice(i + 1),
+                    ])
+                }}
+            />
+        )
+    }
+    let outputHtml = Array.from(generateItemsFrom(inputItems, allowSmelting)).map((itemName) => {
+        return (
+            <div className={"bg-blue-200 rounded my-2"} key={itemName}>
+                {itemName}
+            </div>
+        )
+    })
+
+    let datalist = (
+        <datalist id={"items"}>
+            {allItemNames.map((item) => {
+                return <option key={item} value={item} />
+            })}
+        </datalist>
+    )
+
+    return (
+        <div>
+            <div className={"text-center"}>
+                <a
+                    className={"text-center text-lg font-bold hover:bg-blue-300 rounded"}
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/BurnySc2/Factorio-Train-Station-Blueprint-Creator"
+                >
+                    Source code
+                </a>
+            </div>
+            <div className={"grid grid-cols-2 m-4 gap-x-4"}>
+                <div>
+                    <div className={"m-auto"}>
+                        <div className={"text-xl font-bold"}>Ingredients</div>
+                        <input
+                            id="allowSmelting"
+                            type={"checkbox"}
+                            checked={allowSmelting}
+                            onChange={(e) => {
+                                setAllowSmelting(e.target.checked)
+                            }}
+                        />
+                        <label htmlFor={"allowSmelting"}>Allow smelting?</label>
+                    </div>
+                    <div className={"grid grid-cols-5 gap-x-2 gap-y-1"}>{inputHtml}</div>
+                </div>
+                <div>
+                    <div className={"text-xl font-bold"}>
+                        Items that can be made from ingredients
+                    </div>
+                    {outputHtml}
+                </div>
+                {datalist}
+            </div>
+        </div>
+    )
+}
---
+++ src/constants/helper.ts
@@ -0,0 +1,55 @@
+import itemList from "./itemlist.json"
+
+export let allItemNames = itemList.map((item) => {
+    return item.id
+})
+
+export let recipes = itemList
+    .map((item) => {
+        let ingredients = new Set()
+        if (item.recipe.ingredients.length > 0) {
+            item.recipe.ingredients.forEach((ingredient) => {
+                ingredients.add(ingredient.id)
+            })
+            return { [item.id]: ingredients }
+        }
+        return null
+    })
+    .filter((item) => {
+        return item
+    })
+
+function isSuperset(set: Set<string>, subset: Set<string>): boolean {
+    for (let elem of subset) {
+        if (!set.has(elem)) {
+            return false
+        }
+    }
+    return true
+}
+
+let smeltItems = new Set(["iron-plate", "steel-plate", "copper-plate", "stone-brick"])
+
+export let generateItemsFrom = (availableItems: string[], allowSmelting = false): string[] => {
+    let availableItemsSet = new Set(availableItems)
+    let newItems: Set<string> = new Set()
+    let oldLength = newItems.size
+    let iteration = 0
+    while (iteration === 0 || newItems.size !== oldLength) {
+        iteration += 1
+        oldLength = newItems.size
+        recipes.forEach((item) => {
+            // @ts-ignore
+            let key: string = Object.keys(item)[0]
+            if (!allowSmelting && smeltItems.has(key)) {
+                return
+            }
+            // @ts-ignore
+            let value: Set<string> = item[key]
+            if (isSuperset(availableItemsSet, value)) {
+                newItems.add(key)
+            }
+        })
+    }
+    return [...newItems]
+}
